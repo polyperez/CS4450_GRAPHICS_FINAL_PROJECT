457779f242463c2d9f488cb9b2d3c8a92b157d7b
Victor Perez <vhperez@cpp.edu>
2026-03-18 10:56:37 -0700

    Colored Cube Added

--- src/Main.java
+++ src/Main.java
@@ -24,26 +24,39 @@ public class Main {
     Display.setTitle("CS4450 Final Project");
     Display.create();
     
+    glEnable(GL_DEPTH_TEST);
     initGL();
+    
+    Cube cube = new Cube();
+    float angle = 0;
     
     while(!Display.isCloseRequested()){
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        render();
+        //render();
+        
+        glLoadIdentity();
+        glTranslatef(0,0,-5);
+        glRotatef(angle,1,1,0);
+        
+        System.out.println("drawing cube");
+        cube.draw();// IMPORTANT
+        
+        angle += 0.5f;
         
         Display.update();
-        Display.sync(60);
+        //Display.sync(60);
 }
     
 Display.destroy();
 }
 
-private static void initGL(){
-    int GL_Projection = 0;
-    
-    glMatrixMode(GL_Projection);
+private static void initGL(){  
+    glMatrixMode(GL_PROJECTION); //Case sensitive
     glLoadIdentity();
-    glOrtho(-10,10,-10,10,-10,10);
+    //glOrtho(-10,10,-10,10,-10,10);
     
+    float aspect = 640f / 480f;
+    glFrustum(-aspect, aspect, -1, 1, 1, 100);
     glMatrixMode(GL_MODELVIEW);
             }
 private static void render(){
